e5e249dede7dc00d3953290291f19d7fc565063d
Ryan Alyn Porter <rap@endymion.com>
2009-04-27 13:58:28 -0400

    Added layout helpers and the necessary partials.

---
+++ app/helpers/layout_helper.rb
@@ -0,0 +1,48 @@
+module LayoutHelper
+
+  def block_to_partial(partial_name, options = {}, &block)
+    options.merge!(:body => capture(&block))
+    concat render(:partial => partial_name, :locals => options)
+  end
+
+  def layout_and_theme_stylesheets(layout, options = {}, &block)
+    block_to_partial('layouts/layout_and_theme_stylesheets', options.merge(:layout => layout), &block)
+  end
+
+  def head(options = {}, &block)
+    block_to_partial('layouts/head', options, &block)
+  end
+
+  def frame(options = {}, &block)
+    block_to_partial('layouts/frame', options, &block)
+  end
+
+  def header(options = {}, &block)
+    block_to_partial('layouts/header', options, &block)
+  end
+
+  def column_1(options = {}, &block)
+    block_to_partial('layouts/column_1', options, &block)
+  end
+
+  def column_2(options = {}, &block)
+    block_to_partial('layouts/column_2', options, &block)
+  end
+
+  def column_3(options = {}, &block)
+    block_to_partial('layouts/column_3', options, &block)
+  end
+
+  def footer(options = {}, &block)
+    block_to_partial('layouts/footer', options, &block)
+  end
+
+  def google_analytics_tracker(account_id, options = {})
+    render(:partial => 'layouts/google_analytics_tracker', :locals => options.merge(:account_id => account_id))
+  end
+
+  def image_replacement(div_id, options = {}, &block)
+    block_to_partial('layouts/image_replacement', options.merge(:div_id => div_id), &block)
+  end
+  
+end
