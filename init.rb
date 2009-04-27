102b0fd3f95f6d5e67d7760c60bbe93470a060a0
Ryan Alyn Porter <rap@endymion.com>
2009-04-27 14:27:33 -0400

    Configured init.rb and rails/init.rb to work either as a gemo or a plugin.

--- init.rb
+++ init.rb
@@ -1,3 +1,1 @@
 require File.dirname(__FILE__) + "/rails/init"
-require 'app/helpers/layout_helper.rb'
-ActionView::Base.send :include, LayoutHelper
--- rails/init.rb
+++ rails/init.rb
@@ -1,1 +1,2 @@
-# Include hook code here
+require 'app/helpers/layout_helper'
+ActionView::Base.send :include, LayoutHelper
