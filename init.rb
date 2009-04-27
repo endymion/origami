500513d9e014f3c777102b176ea4c4819be13985
Ryan Alyn Porter <rap@endymion.com>
2009-04-27 13:57:28 -0400

    I would really like to be able to test this plugin but right now it's apparently not meant to be.

--- init.rb
+++ init.rb
@@ -1,1 +1,3 @@
 require File.dirname(__FILE__) + "/rails/init"
+require 'app/helpers/layout_helper.rb'
+ActionView::Base.send :include, LayoutHelper
--- test/chaise_test.rb
+++ test/chaise_test.rb
@@ -1,8 +1,26 @@
+# The following line should not be necessary, but it is because of a bug in 
+# ActiveSupport: https://rails.lighthouseapp.com/projects/8994/tickets/2519-requiring-activesupporttestcase-in-a-vanilla-test-fails-to-run-the-test-case
+require 'test/unit'
+
 require 'test_helper'
 
+require 'test/unit'
+require 'rubygems'
+require 'active_support'
+require 'action_view'
+require "#{File.dirname(__FILE__)}/../init"
+
+require File.dirname(__FILE__) + '/../app/helpers/layout_helper'
+
 class ChaiseTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+
+  include ActionView::Helpers::TagHelper
+  include ActionView::Helpers::CaptureHelper
+  include LayoutHelper
+
+  # I would love to be able to test this plugin but right now it's apparently not meant to be.
+  test "block_to_partial helper" do
+    assert block_to_partial('layouts/head', nil), !nil?
   end
+
 end
