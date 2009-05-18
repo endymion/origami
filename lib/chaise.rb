c46cf67ba0e091337dc05c6faaeea502daf4ce6a
Ryan Alyn Porter <rap@endymion.com>
2009-05-18 14:00:10 -0400

    Moved lib/chaise.rb to lib/origami.rb to get gem installation working properly in Rails.

--- lib/chaise.rb
+++
@@ -1,9 +0,0 @@
-class ApplicationController < ActionController::Base
-
-  # Override render so that it will look for a layout setting.
-  def render(options = {}, extra_options = {}, &block)
-    options.merge!(:layout => @layout) unless @layout.nil?
-    super(options, extra_options, &block)
-  end
-
-end
