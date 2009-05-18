54ac41f9270a3f99b87036da9ae0e32bf784ede3
Ryan Alyn Porter <rap@endymion.com>
2009-05-18 17:25:23 -0400

    Changed body to content.

--- app/helpers/layout_helper.rb
+++ app/helpers/layout_helper.rb
@@ -13,7 +13,7 @@ module LayoutHelper
     block_to_partial('layouts/head', options, &block)
   end
 
-  def body(options = {}, &block)
+  def content(options = {}, &block)
     block_to_partial('layouts/body', options, &block)
   end
 
